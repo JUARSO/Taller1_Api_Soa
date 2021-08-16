a427d744f5130d6b13e754946355d11d22e52c9c
JUARSO <juan09fut.js@gmail.com>
2021-08-16 15:08:48 -0600

    Errores

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 var parqueo = require ("./public/javascripts/Clases/Parqueo");
 
 var createError = require('http-errors');
-var express = require('express');
+var express = require('express'),
+    bodyParser = require("body-parser"),
+    swaggerJsdoc = require("swagger-jsdoc"),
+    swaggerUi = require("swagger-ui-express");
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -56,6 +60,43 @@ module.exports.tecParqueo = tecParqueo;
 module.exports = app;
 module.exports.corsOptions = corsOptions;
 
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "LogRocket Express API with Swagger",
+            version: "0.1.0",
+            description:
+                "This is a simple CRUD API application made with Express and documented with Swagger",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+                name: "LogRocket",
+                url: "https://logrocket.com",
+                email: "info@email.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000/spaces",
+            },
+            {
+                url: "http://localhost:3000/reservations",
+            },
+
+        ],
+    },
+    apis: ["./routes/spaces.js","./routes/reservations.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
 
 https.createServer({  key: fs.readFileSync('./public/security/cert.key'),
                               cert: fs.readFileSync('./public/security/cert.pem')
