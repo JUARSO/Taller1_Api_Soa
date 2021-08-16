1334555b2fc429fdfb517b52a66c8708a5e00bed
Bratan <alfabrayan12@gmail.com>
2021-08-16 17:01:18 -0600

    Comentarios de Swagger

--- app.js
+++ app.js
@@ -34,18 +34,14 @@ const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerDefinition = {
     openapi: '3.0.0',
     info: {
-        title: 'Express API for JSONPlaceholder',
+        title: 'Aplicacion de Parqueos TEC',
         version: '1.0.0',
         description:
-            'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+            'Aplicacion para parqueos que permite hacer CRUD en los espacios y en las reservaciones',
         license: {
             name: 'Licensed Under MIT',
             url: 'https://spdx.org/licenses/MIT.html',
-        },
-        contact: {
-            name: 'JSONPlaceholder',
-            url: 'https://jsonplaceholder.typicode.com',
-        },
+        }
     },
     servers: [
         {
--- routes/spaces.js
+++ routes/spaces.js
@@ -10,30 +10,81 @@ var accepts = require('accepts')
 
 /**
  * @swagger
+ * tags:
+ * name: Spaces
+ * description: API to manage your books.
+ */
+
+/**
+ * @swagger
+ *    components:
+ *        schemas:
+ *            Space:
+ *                type: object
+ *                required:
+ *                    - id
+ *                    - status
+ *                properties:
+ *                       id:
+ *                         type: integer
+ *                         description: El ID del espacio.
+ *                         example: 0
+ *                       state:
+ *                         type: string
+ *                         description: El estado del espacio.
+ *                         example: free
+ *                       description:
+ *                          type: string
+ *                          description: Descripcion del espacio
+ *                          example: Muy Bonito
+ *                       carro:
+ *                          $ref: '#/components/schemas/Carro'
+ *            Carro:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      description: El ID del espacio.
+ *                      example: 0
+ *                  placa:
+ *                      type: string
+ *                      description: La placa del carro.
+ *                      example: abcd123
+ *                  horaIngreso:
+ *                      type: Date
+ *                      description: La hora de ingreso del carro.
+ *                      example: 2021-08-16T21:34:41.750Z
+
+
+ */
+
+/**
+ * @swagger
  * /spaces:
  *   get:
+ *     tags: [Spaces]
  *     summary: Devuelve la lista de espacios disponibles.
  *     description: Devuelve la lista de espacios disponibles.
  *     parameters:
- *       - in: path
+ *       - in: query
  *         name: state
  *         required: false
  *         description: Indica si se debe de filtrar por estado.
  *         schema:
  *           type: string
- *       - in: path
+ *       - in: query
  *         name: limit
  *         required: false
  *         description: Cantidad de elementos a recibir.
  *         schema:
  *           type: string
- *       - in: path
+ *       - in: query
  *         name: offset
  *         required: false
  *         description: Numero de elemento por el cual empezar a filtrar.
  *         schema:
  *           type: string
- *       - in: path
+ *       - in: query
  *         name: fields
  *         required: false
  *         description: Nombres de los campos a devolver.
@@ -47,35 +98,164 @@ var accepts = require('accepts')
  *             schema:
  *                   type: array
  *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: integer
- *                         description: El ID del espacio.
- *                         example: 0
- *                       state:
- *                         type: string
- *                         description: El estado del espacio.
- *                         example: free
- *                       carro:
- *                         type: object
- *                         properties:
- *                          id:
- *                              type: integer
- *                              description: El ID del espacio.
- *                              example: 0
- *                          placa:
- *                              type: string
- *                              description: La placa del carro.
- *                              example: abcd123
- *                          horaIngreso:
- *                              type: Date
- *                              description: La hora de ingreso del carro.
- *                              example: 2021-08-16T21:34:41.750Z
+ *                     $ref: '#/components/schemas/Space'
  *       406:
- *         description: Error. El cliente no acepta json.
+ *         description: El cliente no acepta json.
+ *         content:
+ *   post:
+ *     tags: [Spaces]
+ *     summary: Agrega un espacio nuevo.
+ *     description: Agrega un espacio nuevo.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               description:
+ *                 type: string
+ *                 description: La descripcion del espacio.
+ *                 example: Muy Bonito
+ *     responses:
+ *       200:
+ *         description: El espacio creado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *                  $ref: '#/components/schemas/Space'
+ *       406:
+ *         description: El cliente no acepta json.
  *         content:
  */
+/**
+ * @swagger
+ *   /spaces/{id}:
+ *   get:
+ *     tags: [Spaces]
+ *     summary: Devuelve un espacios.
+ *     description: Devuelve un espacios.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Indica el id de espacio a solicitar.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: La lista de espacios.
+ *         content:
+ *           application/json:
+ *             schema:
+ *                  $ref: '#/components/schemas/Space'
+ *       406:
+ *         description: El cliente no acepta json.
+ *         content:
+ *       400:
+ *         description: No existe el espacio con el ID indicado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *                     type: object
+ *                     properties:
+ *                       message:
+ *                         type: string
+ *                         description: Mensaje de error.
+ *                         example: No existe un espacio con el id indicado
+ *   put:
+ *     tags: [Spaces]
+ *     summary: Edita un espacio existente.
+ *     description: Edita un espacio existente.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Indica el id de espacio a modificar.
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               description:
+ *                 type: string
+ *                 description: La descripcion del espacio.
+ *                 example: Muy Bonito
+ *     responses:
+ *       200:
+ *         description: El espacio modificado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *                  $ref: '#/components/schemas/Space'
+ *       406:
+ *         description: El cliente no acepta json.
+ *         content:
+ *       400:
+ *         description: No existe el espacio con el ID indicado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *                     type: object
+ *                     properties:
+ *                       message:
+ *                         type: string
+ *                         description: Mensaje de error.
+ *                         example: No existe un espacio con el id indicado
+ *   delete:
+ *     tags: [Spaces]
+ *     summary: Elimina un espacio.
+ *     description: Elimina un espacio.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Indica el id de espacio a modificar.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Se eliminó con éxito.
+ *         content:
+ *           application/json:
+ *             schema:
+ *                     type: object
+ *                     properties:
+ *                       message:
+ *                         type: string
+ *                         description: Mensaje.
+ *                         example: Se eliminó con éxito
+ *       400:
+ *         description: No existe el espacio con el ID indicado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *                     type: object
+ *                     properties:
+ *                       message:
+ *                         type: string
+ *                         description: Mensaje de error.
+ *                         example: No existe un espacio con el id indicado
+ *       406:
+ *         description: El cliente no acepta json.
+ *         content:
+ *       409:
+ *         description: El espacio está ocupado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *                     type: object
+ *                     properties:
+ *                       message:
+ *                         type: string
+ *                         description: Mensaje de error.
+ *                         example: El espacio está ocupado
+*/
+/* GET spaces*/
 router.get('/', cors(app.corsOptions), function(req, res, next) {
     if(req.accepts('application/json')){
         const tecParqueo = app.tecParqueo
@@ -114,7 +294,7 @@ router.get('/', cors(app.corsOptions), function(req, res, next) {
     }
 });
 
-/* GET home page. */
+/* GET space*/
 router.get('/:id', cors(app.corsOptions), function(req, res, next) {
     if(req.accepts('application/json')){
         const tecParqueo = app.tecParqueo
@@ -129,7 +309,7 @@ router.get('/:id', cors(app.corsOptions), function(req, res, next) {
     }
 });
 
-/* GET home page. */
+/* POST space*/
 router.post('/', cors(app.corsOptions), function(req, res, next) {
     if(req.accepts('application/json')){
         if(req.body.description === undefined){
@@ -145,7 +325,7 @@ router.post('/', cors(app.corsOptions), function(req, res, next) {
     }
 });
 
-/* GET home page. */
+/* PUT space*/
 router.put('/:id', cors(app.corsOptions), function(req, res, next) {
     if(req.accepts('application/json')){
         const tecParqueo = app.tecParqueo
@@ -165,7 +345,7 @@ router.put('/:id', cors(app.corsOptions), function(req, res, next) {
     }
 });
 
-/* GET home page. */
+/* DELETE space*/
 router.delete('/:id', cors(app.corsOptions), function(req, res, next) {
     if(req.accepts('application/json')){
         const tecParqueo = app.tecParqueo
