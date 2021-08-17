bf533b616cc905d3ad45231b77d7f6750e8ac707
Bratan <alfabrayan12@gmail.com>
2021-08-16 18:16:38 -0600

    Mover comentarios espacios

--- routes/spaces.js
+++ routes/spaces.js
@@ -102,32 +102,49 @@ var accepts = require('accepts')
  *       406:
  *         description: El cliente no acepta json.
  *         content:
- *   post:
- *     tags: [Spaces]
- *     summary: Agrega un espacio nuevo.
- *     description: Agrega un espacio nuevo.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               description:
- *                 type: string
- *                 description: La descripcion del espacio.
- *                 example: Muy Bonito
- *     responses:
- *       200:
- *         description: El espacio creado.
- *         content:
- *           application/json:
- *             schema:
- *                  $ref: '#/components/schemas/Space'
- *       406:
- *         description: El cliente no acepta json.
- *         content:
- */
+ *
+ * */
+
+
+/* GET spaces*/
+router.get('/', cors(app.corsOptions), function(req, res, next) {
+    if(req.accepts('application/json')){
+        const tecParqueo = app.tecParqueo
+        const estadoQuery = req.query.state;
+        const fields = req.query.fields;
+
+        let listaEspacios = [];
+
+        listaEspacios = tecParqueo.listaEspacios;
+        listaEspacios = JSON.parse(JSON.stringify(listaEspacios))
+        if(estadoQuery){
+            listaEspacios = tecParqueo.filtrarEspaciosPorEstado(listaEspacios, estadoQuery)
+        }
+
+        if(fields){
+            if(!fields.includes("state")){
+                listaEspacios.forEach(espacio=>{
+                    delete espacio.estado
+                });
+            }
+            if(!fields.includes("car")){
+                listaEspacios.forEach(espacio=>{
+                    delete espacio.carro
+                });
+            }
+            if(!fields.includes("description")){
+                listaEspacios.forEach(espacio=>{
+                    delete espacio.descripcion
+                });
+            }
+        }
+        listaEspacios = listaEspacios.slice(req.query.offset).slice(0,req.query.limit);
+        res.send(listaEspacios)
+    }else{
+        res.status(406).send()
+    }
+});
+
 /**
  * @swagger
  *   /spaces/{id}:
@@ -163,6 +180,69 @@ var accepts = require('accepts')
  *                         type: string
  *                         description: Mensaje de error.
  *                         example: No existe un espacio con el id indicado
+ */
+router.get('/:id', cors(app.corsOptions), function(req, res, next) {
+    if(req.accepts('application/json')){
+        const tecParqueo = app.tecParqueo
+        const espacio = tecParqueo.obtenerEspacioPorId(req.params.id);
+        if(espacio !== undefined){
+            res.send(espacio)
+        }else{
+            res.status(400).send({message: "No existe un espacio con el id indicado"})
+        }
+    }else{
+        res.status(406).send()
+    }
+});
+
+
+/**
+ * @swagger
+ * /spaces:
+ *   post:
+ *     tags: [Spaces]
+ *     summary: Agrega un espacio nuevo.
+ *     description: Agrega un espacio nuevo.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               description:
+ *                 type: string
+ *                 description: La descripcion del espacio.
+ *                 example: Muy Bonito
+ *     responses:
+ *       200:
+ *         description: El espacio creado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *                  $ref: '#/components/schemas/Space'
+ *       406:
+ *         description: El cliente no acepta json.
+ *         content:
+ */
+router.post('/', cors(app.corsOptions), function(req, res, next) {
+    if(req.accepts('application/json')){
+        if(req.body.description === undefined){
+            res.status(400).send({message: "Debe indicarse una descripcion"})
+        }else{
+            const tecParqueo = app.tecParqueo
+            const espacio = tecParqueo.crearEspacio()
+            espacio.setDescripcion(req.body.description)
+            res.send(espacio)
+        }
+    }else{
+        res.status(406).send()
+    }
+});
+
+/**
+ * @swagger
+ *   /spaces/{id}:
  *   put:
  *     tags: [Spaces]
  *     summary: Edita un espacio existente.
@@ -206,6 +286,29 @@ var accepts = require('accepts')
  *                         type: string
  *                         description: Mensaje de error.
  *                         example: No existe un espacio con el id indicado
+ */
+router.put('/:id', cors(app.corsOptions), function(req, res, next) {
+    if(req.accepts('application/json')){
+        const tecParqueo = app.tecParqueo
+        const espacio = tecParqueo.obtenerEspacioPorId(req.params.id);
+        if(espacio !== undefined){
+            if(req.body.description === undefined){
+                res.status(400).send({message: "Debe indicarse una descripcion"})
+            }else {
+                espacio.setDescripcion(req.body.description)
+                res.send(espacio)
+            }
+        }else{
+            res.status(400).send({message: "No existe un espacio con el id indicado"})
+        }
+    }else{
+        res.status(406).send()
+    }
+});
+
+/**
+ * @swagger
+ *   /spaces/{id}:
  *   delete:
  *     tags: [Spaces]
  *     summary: Elimina un espacio.
@@ -254,99 +357,7 @@ var accepts = require('accepts')
  *                         type: string
  *                         description: Mensaje de error.
  *                         example: El espacio está ocupado
-*/
-/* GET spaces*/
-router.get('/', cors(app.corsOptions), function(req, res, next) {
-    if(req.accepts('application/json')){
-        const tecParqueo = app.tecParqueo
-        const estadoQuery = req.query.state;
-        const fields = req.query.fields;
-
-        let listaEspacios = [];
-
-        listaEspacios = tecParqueo.listaEspacios;
-        listaEspacios = JSON.parse(JSON.stringify(listaEspacios))
-        if(estadoQuery){
-            listaEspacios = tecParqueo.filtrarEspaciosPorEstado(listaEspacios, estadoQuery)
-        }
-
-        if(fields){
-            if(!fields.includes("state")){
-                listaEspacios.forEach(espacio=>{
-                    delete espacio.estado
-                });
-            }
-            if(!fields.includes("car")){
-                listaEspacios.forEach(espacio=>{
-                    delete espacio.carro
-                });
-            }
-            if(!fields.includes("description")){
-                listaEspacios.forEach(espacio=>{
-                    delete espacio.descripcion
-                });
-            }
-        }
-        listaEspacios = listaEspacios.slice(req.query.offset).slice(0,req.query.limit);
-        res.send(listaEspacios)
-    }else{
-        res.status(406).send()
-    }
-});
-
-/* GET space*/
-router.get('/:id', cors(app.corsOptions), function(req, res, next) {
-    if(req.accepts('application/json')){
-        const tecParqueo = app.tecParqueo
-        const espacio = tecParqueo.obtenerEspacioPorId(req.params.id);
-        if(espacio !== undefined){
-            res.send(espacio)
-        }else{
-            res.status(400).send({message: "No existe un espacio con el id indicado"})
-        }
-    }else{
-        res.status(406).send()
-    }
-});
-
-/* POST space*/
-router.post('/', cors(app.corsOptions), function(req, res, next) {
-    if(req.accepts('application/json')){
-        if(req.body.description === undefined){
-            res.status(400).send({message: "Debe indicarse una descripcion"})
-        }else{
-            const tecParqueo = app.tecParqueo
-            const espacio = tecParqueo.crearEspacio()
-            espacio.setDescripcion(req.body.description)
-            res.send(espacio)
-        }
-    }else{
-        res.status(406).send()
-    }
-});
-
-/* PUT space*/
-router.put('/:id', cors(app.corsOptions), function(req, res, next) {
-    if(req.accepts('application/json')){
-        const tecParqueo = app.tecParqueo
-        const espacio = tecParqueo.obtenerEspacioPorId(req.params.id);
-        if(espacio !== undefined){
-            if(req.body.description === undefined){
-                res.status(400).send({message: "Debe indicarse una descripcion"})
-            }else {
-                espacio.setDescripcion(req.body.description)
-                res.send(espacio)
-            }
-        }else{
-            res.status(400).send({message: "No existe un espacio con el id indicado"})
-        }
-    }else{
-        res.status(406).send()
-    }
-});
-
-/* DELETE space*/
-router.delete('/:id', cors(app.corsOptions), function(req, res, next) {
+ */router.delete('/:id', cors(app.corsOptions), function(req, res, next) {
     if(req.accepts('application/json')){
         const tecParqueo = app.tecParqueo
         const espacio = tecParqueo.obtenerEspacioPorId(req.params.id)
